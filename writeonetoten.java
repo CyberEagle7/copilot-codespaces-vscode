bba8ad351a55780b0c36e09b69a602c4bcf58a7c
CyberEagle7 <himanshuupadhyay740@gmail.com>
2026-05-30 20:59:43 +0530

    Implement Java program to print numbers 1 to 10
    Corrected the class name and fixed the syntax for printing.

---
+++ writeonetoten.java
@@ -0,0 +1,25 @@
+// We write the code in java for writing 1 to 10.
+public class writeonetoten {
+  public static void main(String [} args){
+    int a = 1; 
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int e = 5;
+    int f = 6;
+    int g = 7;
+    int h = 8;
+    int i = 9;
+    int j = 10;
+    system.out.println(a);
+    system.out.println(b);
+    system.out.println(c);
+    system.out.println(d);
+    system.out.println(e);
+    system.out.println(f);
+    system.out.println(g);
+    system.out.println(h);
+    system.out.println(i);
+    system.out.println(j);
+  }
+}
