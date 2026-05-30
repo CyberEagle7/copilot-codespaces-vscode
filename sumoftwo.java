6705878bee135345d83cc962fabf76bf963401d6
CyberEagle7 <himanshuupadhyay740@gmail.com>
2026-05-30 20:56:04 +0530

    Implement sum of two integers in sumoftwo.java
    Fix syntax errors and implement addition of two integers.

---
+++ sumoftwo.java
@@ -0,0 +1,8 @@
+// We write the code in java for adding two random integer values.
+public class sumoftwo {
+  public static void main(String [} args){
+    int a = 5; 
+    int b = 6;
+    system.out.println(a+b);
+  }
+}
