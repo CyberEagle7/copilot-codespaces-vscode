cf73006df76aba8026445dc89a4e824e09284f87
CyberEagle7 <himanshuupadhyay740@gmail.com>
2026-05-30 21:01:40 +0530

    Create onetoteninloop.java

---
+++ onetoteninloop.java
@@ -0,0 +1,8 @@
+// We write the code in java to write 1 to 10 using loop.
+public class onetoteninloop {
+  public static void main(String [} args){
+    for(int i = 1; i < 11; i++){
+      system.out.println(i);
+    }
+  }
+}
